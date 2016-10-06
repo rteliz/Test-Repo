72a46f43bb13f79a1d577e50ec1e75abdb2fe123
rteliz <9rteliz@stpatrick.org>
2016-10-06 14:13:04 -0500

    started with octocat

---
+++ octocats.js
@@ -0,0 +1,17 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function octocat(){
+  var Name
+  var Arms
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
